42d9571776772f2ea3214d598b33278aeb34e835
adarshkumar714 <adarshkumar20012704@gmail.com>
2024-05-12 18:03:33 +0530

    completed basic flask app

--- main/app.py
+++ main/app.py
@@ -1,15 +1,39 @@
+import os
+from dotenv import load_dotenv
 from flask import *
 import main_main as utils
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
-def main():
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
     files = request.files.getlist('file')
-    queries = []
+    queries = request.form.getlist('strings[]')
+
+    print(files[0])
+    for file in files:
+        file.save(os.path.join('docs', file.filename))
+    
+    print(queries)
+
+    return "uploaded"
+
+@app.route('/api', methods=['POST'])
+def api():
+    files = request.files.getlist('file')
+    queries = [request.form.get('query')]
 
     for file in files:
-        file.save(os.path.join('docs', filename))
+        file.save(os.path.join('docs', file.filename))
 
     # for file in files:
     #     utils.save_docs(file)
@@ -23,12 +47,18 @@ def main():
     responses = []
 
     for q in queries:
-        res = utils.generate_response(q)
+        res = utils.generate_response(query_engine, q)
         responses.append(res)
     
-    return responses
+    for file in files:
+        filepath = os.path.join('docs', file.filename)
+        os.remove(filepath)
+    
+    return str(responses[0])
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
--- main/main_main.py
+++ main/main_main.py
@@ -1,7 +1,4 @@
-
-
-
-
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
 def save_docs():
     # saving all files in /docs folder
@@ -9,21 +6,31 @@ def save_docs():
 
 def load_docs():
     # loading all files in /docs using SimpleDirectoryReader
-    return
+    debug('loading doc')
+    documents = SimpleDirectoryReader('docs').load_data()
+    return documents
 
-def create_index():
+def create_index(documents):
     # creating index using VectorStoreIndex and returning that index
-    return
+    debug('making index')
+    index = VectorStoreIndex.from_documents(documents)
+    return index
 
-def create_query_engine():
+def create_query_engine(index):
     # crating query engine using index.as_query_engine()
-    return
+    debug('generating query engine')
+    query_engine = index.as_query_engine()
+    return query_engine
 
-def generate_response():
+def generate_response(query_engine, query):
     # generate response for one query using query engine
-    return
+    debug('generating response')
+    response = query_engine.query(query)
+    # debug('response:', response)
+    return response
 
-
+def debug(x):
+    print('[+]', x)
 
 
 
